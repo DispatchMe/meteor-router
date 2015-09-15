bc72a89125cb696db64dec2e66331e05d598d464
Jonathan Perl <perl.jonathan@gmail.com>
2015-09-15 15:42:26 -0400

    Bump and release

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'dispatch:router',
   summary: 'A lightweight wrapper around FlowRouter to emit events and track direction.',
   git: 'https://github.com/DispatchMe/meteor-router.git',
-  version: '0.0.8'
+  version: '1.0.0'
 });
 
 Package.onUse(function (api) {
